b4bc9a178f07020a4278f45fdc3771da2fb1f3e9
Alex1986nder <99564342+Alex1986nder@users.noreply.github.com>
2022-07-31 16:08:39 +0300

    правки

--- projects/map/index.js
+++ projects/map/index.js
@@ -1,10 +1,9 @@
-// import './index.html';
+import './index.html';
+import './style.css';
+
 ymaps.ready(init);
-let storage = localStorage;
-let clusterer;
-let coords;
-let myMap, placemark;
-const result = JSON.parse(storage.data || '{}');
+let myMap, placemark, coords, clusterer;
+const result = JSON.parse(localStorage.data || '{}');
 function init() {
   myMap = new ymaps.Map(
     'map',
@@ -18,37 +17,54 @@ function init() {
   );
 
   myMap.events.add('click', function (e) {
-    myMap.balloon.close();
+    let coords = e.get('coords');
+    myMap.balloon.open(coords, {
+      content:
+        '<div id = "reviews"; style = "height: 350px;">' +
+        `<div id = "review__list"; style = "height: 100px; overflow: scroll;"></div>` +
+        '<h1>Отзыв</h1>' +
+        '<ul style = "padding: 0; list-style-type: none;">' +
+        '<li><input id = "myName" placeholder="Укажите Ваше имя"></li>' +
+        '<li><input id = "place" placeholder="Укажите место"></li>' +
+        '<li><textarea id = "review" style = "resize: none;" placeholder="Оставьте отзыв"></textarea>' +
+        '<li><button id = "btn">Добавить</button></li>' +
+        '<ul>' +
+        '</div>',
+    });
   });
 
-  myMap.events.add('click', function (e) {
-    if (!myMap.balloon.isOpen()) {
-      let coords = e.get('coords');
-      myMap.balloon.open(coords, {
-        content:
-          '<div id = "reviews"; style = "height: 300px;">' +
-          '<div id = "review__list"; style = "height: 100px; overflow: scroll;"></div>' +
-          '<h1>Отзыв</h1>' +
-          '<ul style = "padding: 0; list-style-type: none;">' +
-          '<li><input id = "myName" placeholder="Укажите Ваше имя"></li>' +
-          '<li><input id = "place" placeholder="Укажите место"></li>' +
-          '<li><textarea id = "review" style = "resize: none;" placeholder="Оставьте отзыв"></textarea>' +
-          '<li><button id = "btn">Добавить</button></li>' +
-          '<ul>' +
-          '</div>',
-      });
-    }
-  });
   clusterer = new ymaps.Clusterer({
     groupByCoordinates: true,
     clusterDisableClickZoom: true,
     clusterOpenBalloonOnClick: false,
   });
+
   clusterer.events.add('click', (e) => {
     const coords = e.get('target').geometry.getCoordinates();
     myMap.balloon.open(coords, createForm());
+    // myMap.balloon.open(coords, {
+    //   content:
+    //     '<div id = "reviews"; style = "height: 350px;">' +
+    //     `<div id = "review__list"; style = "height: 100px; overflow: scroll;">${createForm(result)}</div>` +
+    //     '<h1>Отзыв</h1>' +
+    //     '<ul style = "padding: 0; list-style-type: none;">' +
+    //     '<li><input id = "myName" placeholder="Укажите Ваше имя"></li>' +
+    //     '<li><input id = "place" placeholder="Укажите место"></li>' +
+    //     '<li><textarea id = "review" style = "resize: none;" placeholder="Оставьте отзыв"></textarea>' +
+    //     '<li><button id = "btn">Добавить</button></li>' +
+    //     '<ul>' +
+    //     '</div>',
+    // });
   });
 
+  let arr = [];
+
+  const storage = JSON.parse(localStorage.getItem('reviews')) || [];
+  const newStorage = [...storage]
+  // console.log(storage);
+  newStorage.forEach((reviews) => {
+    createPlacemark(reviews.coords);
+  });
   myMap.geoObjects.add(clusterer);
 
   document.body.addEventListener('click', onClick);
@@ -56,49 +72,64 @@ function init() {
   myMap.events.add('click', function (e) {
     return (coords = e.get('coords'));
   });
-
   function onClick(e) {
     if (e.target.id === 'btn') {
       const myName = document.querySelector('#myName');
       const place = document.querySelector('#place');
       const review = document.querySelector('#review');
 
-      storage.data = JSON.stringify({
-        name: myName.value,
-        place: place.value,
-        review: review.value,
-      });
-      createForm(coords, result);
+      localStorage.data = JSON.stringify([
+        {
+          coords: coords,
+          reviews: [
+            {
+              name: myName.value,
+              place: place.value,
+              review: review.value,
+            },
+          ],
+        },
+      ]);
+      arr.push(localStorage.data);
+      // console.log(arr);
+      localStorage.setItem('reviews', JSON.stringify(arr));
+      // console.log(arr);
+      createForm(result);
       createPlacemark(coords, createForm);
+      // myMap.balloon.close();
     }
   }
-
-  // const reviews = document.querySelector('#reviews');
-  function createForm(coords, result) {
+  // createForm()
+  function createForm(result) {
     const div = document.createElement('div');
     const reviewList = document.querySelector('#review__list');
-    // const root = document.createElement('div');
-    // root.innerHTML = reviews.innerHTML;
-    reviewList.id.coords = JSON.stringify(coords);
-    result = JSON.parse(storage.data || '{}');
+    result = JSON.parse(localStorage.data || '{}');
+    // console.log(result);
     div.innerHTML = `
-    <div><b>${result.name}</b>[${result.place}]</div><div>${result.review}</div>`;
+    <div><b>${result[0].reviews[0].name}</b>[${result[0].reviews[0].place}]</div><div>${result[0].reviews[0].review}</div>`;
     reviewList.appendChild(div);
-
-     reviewList.innerHTML 
-    // myMap.balloon.setData(reviews.innerHTML);
-    //  reviewList;
+    // console.log(div);
+    console.log(reviewList);
   }
 
   function createPlacemark(coords, result) {
     placemark = new ymaps.Placemark(coords);
     placemark.events.add('click', (e) => {
-      // const reviews = document.querySelector('#reviews');
       const coords = e.get('target').geometry.getCoordinates();
-      // createForm(coords, result)
-      // myMap.balloon.setData(form.innerHTML);
-      // myMap.balloon.getData(reviews.innerHTML);
-      myMap.balloon.open(coords,createForm());
+      myMap.balloon.open(coords, createForm());
+      // myMap.balloon.open(coords, {
+      //   content:
+      //     '<div id = "reviews"; style = "height: 350px;">' +
+      //     `<div id = "review__list"; style = "height: 100px; overflow: scroll;">${createForm(result)}</div>` +
+      //     '<h1>Отзыв</h1>' +
+      //     '<ul style = "padding: 0; list-style-type: none;">' +
+      //     '<li><input id = "myName" placeholder="Укажите Ваше имя"></li>' +
+      //     '<li><input id = "place" placeholder="Укажите место"></li>' +
+      //     '<li><textarea id = "review" style = "resize: none;" placeholder="Оставьте отзыв"></textarea>' +
+      //     '<li><button id = "btn">Добавить</button></li>' +
+      //     '<ul>' +
+      //     '</div>',
+      // });
     });
     clusterer.add(placemark);
   }
