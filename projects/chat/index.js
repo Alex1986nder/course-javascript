be4768d581ec75e7e8ab7c79d02664d8393573b0
Alex1986nder <99564342+Alex1986nder@users.noreply.github.com>
2022-08-08 04:20:24 +0300

    на проверку проект - чат

--- projects/chat/index.js
+++ projects/chat/index.js
@@ -1,5 +1,6 @@
 import './index.html';
-import WS from './ws.mjs';
+import WSClient from './wsClient';
+const wsClient = new WSClient(`ws://${location.host}/mega-chat-3/ws`);
 
 //авторизация
 
@@ -8,32 +9,24 @@ const chatName = document.querySelector('.login-name-input');
 const entryWindow = document.querySelector('#login');
 const chatWindow = document.querySelector('#main');
 const sendButton = document.querySelector('.send-button');
+const userPhoto = document.querySelector('.user-photo');
 
+const userName = document.querySelector('[data-role=user-name]');
+
+const userList = document.querySelector('.user-list');
 chatEntryBtn.addEventListener('click', (e) => {
-  const userList = document.querySelector('.user-list');
   const newChatUser = document.createElement('div');
   const nameValue = chatName.value;
-  newChatUser.classList.add('.user-name');
+  // newChatUser.classList.add('.user-name');
   if (nameValue !== '') {
     entryWindow.classList.add('hidden');
     chatWindow.classList.remove('hidden');
-    const userName = document.querySelector('[data-role=user-name]');
     userName.textContent = nameValue;
     userList.textContent = nameValue;
-    // const socket = new WebSocket('ws:/localhost:2000');
-
-    // socket.onopen = function () {
-    //   const ws = new WS('client', socket).ws;
-
-    //   ws.subscribe('user:connect', (data) => {
-    //     console.log(data);
-    //   });
-
-    //   ws.subscribe('message:hello', (data) => {
-    //     console.log(data);
-    //   });
-    // };
-    onMessage(type, from, data );
+    userPhoto.style.backgroundImage = `url(/projects/chat/photos/${
+      chatName.value
+    }.png?t=${Date.now()})`;
+    onMessage();
   }
   userList.appendChild(newChatUser);
   // chatName.value = '';
@@ -41,37 +34,77 @@ chatEntryBtn.addEventListener('click', (e) => {
 
 //отправка сообщения
 
+const messagesСontainer = document.querySelector('.messages-container');
 sendButton.addEventListener('click', (e) => {
   const messageInput = document.querySelector('.message-input');
-  const messagesСontainer = document.querySelector('.messages-container');
   const newMessage = document.createElement('div');
   newMessage.textContent = messageInput.value;
+  newMessage.classList.add('message-item', 'message-item-system');
   messagesСontainer.appendChild(newMessage);
+  messagesСontainer.scrollTop = messagesСontainer.scrollHeight;
   // messageInput.value = '';
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, 0);
+  const minutes = String(date.getMinutes()).padStart(2, 0);
+  const time = `${hours}:${minutes}`;
+  newMessage.classList.add('message-item');
+  newMessage.innerHTML = `
+  <div class="message-item-left">
+      <div
+      style="background-image: url(/mega-chat-3/photos/${
+        chatName.value
+      }.png?t=${Date.now()})" 
+      class="message-item-photo" data-role="user-avatar" data-user=${
+        chatName.value
+      }></div>
+  </div>
+  <div class="message-item-right">
+    <div class="message-item-header">
+        <div class="message-item-header-name">${chatName.value}</div>
+        <div class="message-item-header-time">${time}</div>
+    </div>
+    <div class="message-item-text">${messageInput.value}</div>
+  </div>
+  `;
 });
 
 //функция: сообщает всем о...
 
-function onMessage({ type, from, data }) {
-  // console.log(type, from, data);
-
-  if (type === 'user:connect') {
-    // this.ui.userList.add(from);
+function onMessage(type, from, data) {
+  console.log(`${chatName.value} вошел в чат`);
+  if (type === 'hello') {
     messagesСontainer.addSystemMessage(`${chatName.value} вошел в чат`);
+  } else if (type === 'user-list') {
+    for (const item of data) {
+      userList.add(item);
+    }
   }
-  // else if (type === 'user-list') {
-  //   for (const item of data) {
-  //     this.ui.userList.add(item);
-  //   }
-  // } else if (type === 'bye-bye') {
-  //   this.ui.userList.remove(from);
-  //   this.ui.messageList.addSystemMessage(`${from} вышел из чата`);
-  // } else if (type === 'text-message') {
-  //   this.ui.messageList.add(from, data.message);
-  // } else if (type === 'photo-changed') {
-  //   const avatars = document.querySelectorAll(
-  //     `[data-role=user-avatar][data-user=${data.name}]`
-  // );
+}
 
-  // }
+// перетаскивание фото
+
+userPhoto.addEventListener('dragover', (e) => {
+  if (e.dataTransfer.items.length && e.dataTransfer.items[0].kind === 'file') {
+    e.preventDefault();
+  }
+});
+
+userPhoto.addEventListener('drop', (e) => {
+  const file = e.dataTransfer.items[0].getAsFile();
+  const reader = new FileReader();
+
+  reader.readAsDataURL(file);
+  reader.addEventListener('load', () => onUpload(reader.result));
+  e.preventDefault();
+});
+
+function onUpload(data) {
+  userPhoto.style.backgroundImage = `url(${data})`;
+  fetch('/mega-chat-3/upload-photo', {
+    method: 'post',
+    body: JSON.stringify({
+      name: chatName.value,
+      image: data,
+    }),
+  });
 }
--- projects/chat/server/index.js
+++ projects/chat/server/index.js
@@ -1,37 +1,110 @@
-import { WebSocketServer } from 'ws';
-import http from 'http';
-import WS from '../ws.mjs';
-const wss = new WebSocketServer({ noServer: true });
+// не забудьте сделать npm install ;)
 
-const server = http.createServer((req, res) => {
-  wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-});
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const { Server } = require('ws');
 
-function onSocketConnect(client) {
-  const ws = new WS('server', client).ws;
+function readBody(req) {
+  return new Promise((resolve, reject) => {
+    let dataRaw = '';
 
-  ws.sendMessage('user:connect', {
-    user: {
-      name: `${chatName.value}`,
-    },
-  });
-
-  ws.sendMessage('message:hello', {
-    user: {
-      name: `${chatName.value}`,
-    },
-  });
-
-  ws.subscribe('user:connect', (data) => {
-    console.log(data);
-    ws.sendMessage('message:hello', {
-      user: {
-        name: `${chatName.value}`,
-      },
-    });
+    req.on('data', (chunk) => (dataRaw += chunk));
+    req.on('error', reject);
+    req.on('end', () => resolve(JSON.parse(dataRaw)));
   });
 }
 
-server.listen(2000, () => {
-  console.log('listen....');
+const server = http.createServer(async (req, res) => {
+  try {
+    if (/\/photos\/.+\.png/.test(req.url)) {
+      const [, imageName] = req.url.match(/\/photos\/(.+\.png)/) || [];
+      const fallBackPath = path.resolve(__dirname, '../no-photo.png');
+      const filePath = path.resolve(__dirname, '../photos', imageName);
+
+      if (fs.existsSync(filePath)) {
+        return fs.createReadStream(filePath).pipe(res);
+      } else {
+        return fs.createReadStream(fallBackPath).pipe(res);
+      }
+    } else if (req.url.endsWith('/upload-photo')) {
+      const body = await readBody(req);
+      const name = body.name.replace(/\.\.\/|\//, '');
+      const [, content] = body.image.match(/data:image\/.+?;base64,(.+)/) || [];
+      const filePath = path.resolve(__dirname, '../photos', `${name}.png`);
+
+      if (name && content) {
+        fs.writeFileSync(filePath, content, 'base64');
+
+        broadcast(connections, { type: 'photo-changed', data: { name } });
+      } else {
+        return res.end('fail');
+      }
+    }
+
+    res.end('ok');
+  } catch (e) {
+    console.error(e);
+    res.end('fail');
+  }
 });
+const wss = new Server({ server });
+const connections = new Map();
+
+wss.on('connection', (socket) => {
+  connections.set(socket, {});
+
+  socket.on('message', (messageData) => {
+    const message = JSON.parse(messageData);
+    let excludeItself = false;
+
+    if (message.type === 'hello') {
+      excludeItself = true;
+      connections.get(socket).userName = message.data.name;
+      sendMessageTo(
+        {
+          type: 'user-list',
+          data: [...connections.values()].map((item) => item.userName).filter(Boolean),
+        },
+        socket
+      );
+    }
+
+    sendMessageFrom(connections, message, socket, excludeItself);
+  });
+
+  socket.on('close', () => {
+    sendMessageFrom(connections, { type: 'bye-bye' }, socket);
+    connections.delete(socket);
+  });
+});
+
+function sendMessageTo(message, to) {
+  to.send(JSON.stringify(message));
+}
+
+function broadcast(connections, message) {
+  for (const connection of connections.keys()) {
+    connection.send(JSON.stringify(message));
+  }
+}
+
+function sendMessageFrom(connections, message, from, excludeSelf) {
+  const socketData = connections.get(from);
+
+  if (!socketData) {
+    return;
+  }
+
+  message.from = socketData.userName;
+
+  for (const connection of connections.keys()) {
+    if (connection === from && excludeSelf) {
+      continue;
+    }
+
+    connection.send(JSON.stringify(message));
+  }
+}
+
+server.listen(8282);
